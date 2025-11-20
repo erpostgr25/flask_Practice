35833e650a8892883c2615722b8c3d39682e4682
Shashi Kant <er.shashi09@gmail.com>
2025-11-20 13:44:03 +0530

    Added CICD supported codes

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Create .env file for MONGO DB connection"
-echo "MONGO_URI=mongodb+srv://ershashi09_db_user:XXXXX@travel-memory-cluster.qm5fqgo.mongodb.net/students" > .env
+echo "MONGO_URI=mongodb+srv://ershashi09_db_user:M3dO9l7JkOttJeu6@travel-memory-cluster.qm5fqgo.mongodb.net/students" > .env
 
 echo "Starting deployment..."
 # Stop any already running instance
 pkill -f "python3 app.py" 2>/dev/null || true
 # Start the application in background
 setsid nohup python3 app.py > app.log 2>&1 &
-
+sleep 2
 echo "Application deployed to staging environment!"
