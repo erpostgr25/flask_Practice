3e25fd06f6beeb9cad0b824637e1ac1a52b3fe4d
Shashi Kant <er.shashi09@gmail.com>
2025-11-20 14:18:12 +0530

    Added CICD supported codes

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,8 @@
 
 # Create .env file for MONGO DB connection"
 echo "MONGO_URI=mongodb+srv://ershashi09_db_user:M3dO9l7JkOttJeu6@travel-memory-cluster.qm5fqgo.mongodb.net/students" > .env
-
+echo "Stop deployment if already running..."
+sudo systemctl stop flaskapp
 echo "Starting deployment..."
-# Stop any already running instance
-pkill -f "python3 app.py" 2>/dev/null || true
-# Start the application in background
-setsid nohup python3 app.py > app.log 2>&1 < /dev/null &
-sleep 2
+sudo systemctl start flaskapp
 echo "Application deployed to staging environment!"
