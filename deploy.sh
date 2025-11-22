a4e6a94bad9f0871ffc6c3bbd5ffb149cd34cbff
Shashi Kant <er.shashi09@gmail.com>
2025-11-22 14:32:12 +0530

    Added CICD supported codes

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # Create .env file for MONGO DB connection"
 echo "MONGO_URI=mongodb+srv://XXXXX:XXXXX@travel-memory-cluster.qm5fqgo.mongodb.net/students" > .env
 echo "Stop deployment if already running..."
+sudo systemctl daemon-reload
 sudo systemctl stop flaskapp
 echo "Starting deployment..."
 sudo systemctl start flaskapp
